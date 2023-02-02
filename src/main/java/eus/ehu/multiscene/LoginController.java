eac7a9ac0214c87fd58a240b0628df509ac21894
Juanan Pereira <juanan.pereira@ehu.eus>
2023-02-02 17:38:25 +0100

    Initial commit

---
+++ src/main/java/eus/ehu/multiscene/LoginController.java
@@ -0,0 +1,26 @@
+package eus.ehu.multiscene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void loginClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module eus.ehu.multiscene {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens eus.ehu.multiscene to javafx.fxml;
+    exports eus.ehu.multiscene;
+}
